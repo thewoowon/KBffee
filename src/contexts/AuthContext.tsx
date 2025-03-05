da22a60121aefde11500c01f6d8235b86ef09cef
thewoowon <thewoowon@naver.com>
2025-03-06 07:32:03 +0900

    feat: auth context filled in somethings....

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,10 @@ type AuthContextType = {
   isAuthenticated: boolean;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
   initializeAuth: () => Promise<void>;
+  mode: 'supervisor' | 'client';
+  setMode: React.Dispatch<React.SetStateAction<'supervisor' | 'client'>>;
+  storeCode: string | null;
+  initStoreCode: React.Dispatch<React.SetStateAction<string | null>>;
 };
 
 export const AuthContext = createContext<AuthContextType | undefined>(
@@ -14,6 +18,8 @@ export const AuthProvider: React.FC<{children: React.ReactNode}> = ({
   children,
 }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [mode, setMode] = useState<'supervisor' | 'client'>('supervisor');
+  const [storeCode, initStoreCode] = useState<string | null>(null);
 
   const initializeAuth = async () => {
     const isValid = false;
@@ -22,7 +28,15 @@ export const AuthProvider: React.FC<{children: React.ReactNode}> = ({
 
   return (
     <AuthContext.Provider
-      value={{isAuthenticated, setIsAuthenticated, initializeAuth}}>
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        initializeAuth,
+        mode,
+        setMode,
+        storeCode,
+        initStoreCode,
+      }}>
       {children}
     </AuthContext.Provider>
   );
