b00c08299bb8d4b81ea56a3a299456121bccc3a5
thewoowon <thewoowon@naver.com>
2025-03-07 23:58:02 +0900

    feat: update hooks added

--- src/hooks/useFirestore.ts
+++ src/hooks/useFirestore.ts
@@ -1,4 +1,3 @@
-import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore/lib';
 import {
   doc,
   getFirestore,
@@ -14,6 +13,7 @@ const useFirestore = () => {
       const db = getFirestore();
 
       const usersRef = collection(db, 'users');
+      const termsRef = collection(db, 'terms');
 
       const date = new Date().toISOString();
 
@@ -22,6 +22,12 @@ const useFirestore = () => {
         last_used: date.split('T')[0],
         level: 0,
         stamps: 0,
+      });
+
+      // 회원가입 시 동시에 약관 동의 처리
+      await setDoc(doc(termsRef, userId), {
+        agreed: true,
+        date,
       });
 
       console.log('✅ User added successfully!');
@@ -49,6 +55,17 @@ const useFirestore = () => {
     } catch (error) {
       console.error('Error getting document:', error);
       return undefined;
+    }
+  }
+
+  async function updateUser(userId: string, data: any) {
+    try {
+      const db = getFirestore();
+      const userRef = doc(db, 'users', userId);
+      await updateDoc(userRef, data);
+      console.log('User updated successfully!');
+    } catch (error) {
+      console.error('Error updating document:', error);
     }
   }
 
@@ -82,7 +99,18 @@ const useFirestore = () => {
     }
   }
 
-  return {addUser, getUser, getStores, enterNumber};
+  async function updateSession(sessionId: string, data: any) {
+    try {
+      const db = getFirestore();
+      const sessionRef = doc(db, 'sessions', sessionId);
+      await updateDoc(sessionRef, data);
+      console.log('Session updated successfully!');
+    } catch (error) {
+      console.error('Error updating document:', error);
+    }
+  }
+
+  return {addUser, getUser, updateUser, getStores, enterNumber, updateSession};
 };
 
 export default useFirestore;
