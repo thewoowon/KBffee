4a7a13897182fbf9fdd2fd46b180211f0dad76cf
thewoowon <thewoowon@naver.com>
2025-03-18 20:48:23 +0900

    feat: 메인화면 퍼블리실

--- src/screens/auth/ModeSelectionScreen.tsx
+++ src/screens/auth/ModeSelectionScreen.tsx
@@ -21,21 +21,21 @@ const ModeSelectionScreen = ({navigation, route}: any) => {
         translucent={false}
       />
       <SafeAreaView style={styles.backgroundStyle}>
-        <View style={styles.container}>
+        <View style={styles.wrapper}>
           <View style={styles.flexBox}>
             <Pressable
               style={styles.modeContainer}
               onPress={() => {
                 handleSignIn('supervisor');
               }}>
-              <Text>관리자</Text>
+              <Text style={styles.buttonText}>관리자</Text>
             </Pressable>
             <Pressable
               style={styles.modeContainer}
               onPress={() => {
                 handleSignIn('client');
               }}>
-              <Text>고객</Text>
+              <Text style={styles.buttonText}>고객</Text>
             </Pressable>
           </View>
         </View>
@@ -47,6 +47,9 @@ const ModeSelectionScreen = ({navigation, route}: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     paddingLeft: 20,
     paddingRight: 20,
   },
@@ -55,36 +58,32 @@ const styles = StyleSheet.create({
   },
   flexBox: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     gap: 20,
   },
   modeContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 20,
-    backgroundColor: '#f5f5f5',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#e5e5e5',
-  },
-  header: {
     flex: 1,
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 11,
-    paddingBottom: 11,
-    maxHeight: 50,
-    borderBottomWidth: 1,
-    borderColor: '#F2F4F6',
+    paddingVertical: 100,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    // shadow
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 10,
   },
-  headerText: {
-    color: '#181818',
-    fontSize: 18,
+  buttonText: {
+    color: '#191D2B',
+    fontSize: 20,
     fontFamily: 'Pretendard-SemiBold',
   },
 });
